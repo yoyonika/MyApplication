0011d40050d879b5cf8c3fd8873ebab9f8f0cac7
Gamers CI <ioa_1108@yahoo.co.uk>
2019-02-16 22:26:43 +0000

    kotlin

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    @Override
+    protected fun onCreate(savedInstanceState: Bundle) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
